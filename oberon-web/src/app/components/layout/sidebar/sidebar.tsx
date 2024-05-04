24d79df5ba6ba0c6cc569e307c2e5185b8e1a0db
barbarabrito <barbaramabrito@gmail.com>
2024-05-04 13:01:34 -0300

    fix get-rss-feed response type, fix folders ui

--- oberon-web/src/app/components/layout/sidebar/sidebar.tsx
+++ oberon-web/src/app/components/layout/sidebar/sidebar.tsx
@@ -5,19 +5,19 @@ import useBoundStore from "@/app/stores/store"
 import { useEffect, useState } from "react"
 import { FaFolder } from "react-icons/fa"
 import { PiUserCircleFill } from "react-icons/pi"
-import * as Collapsible from '@radix-ui/react-collapsible';
+import * as Collapsible from "@radix-ui/react-collapsible"
 
 type Folders = {
-  id: number;
-  name: number;
+  id: number
+  name: number
   user_rss_feeds: UserFeed[]
-}[];
+}[]
 
 type UserFeed = {
   id: number
   name: string
   url: string
-};
+}
 
 const Sidebar = () => {
   const [folders, setFolders] = useState<Folders>([])
@@ -26,27 +26,26 @@ const Sidebar = () => {
 
   async function getSite(feedUrl: string) {
     const feed = await getRssFeed(feedUrl)
-    setFeed(feed.rss)
+    if (feed) setFeed(feed.rss)
   }
 
   useEffect(() => {
     async function fetchUserFolders() {
       try {
-        const response = await fetch('/mocks/folders.json');
-        if (!response.ok) throw new Error('Failed to fetch folders');
+        const response = await fetch("/mocks/folders.json")
+        if (!response.ok) throw new Error("Failed to fetch folders")
 
-        const folders = await response.json();
-        setFolders(folders);
-
+        const folders = await response.json()
+        setFolders(folders)
       } catch (error) {
-        console.error('Error fetching folders:', error);
+        console.error("Error fetching folders:", error)
       }
     }
-    fetchUserFolders();
+    fetchUserFolders()
   }, [])
 
   return (
-    <div className="flex flex-grow-0 flex-col h-screen min-w-80 p-2 shadow-md bg-zinc-700">
+    <div className="flex flex-grow-0 flex-col h-screen min-w-80 p-2 shadow-md bg-zinc-800 px-4">
       <span className="text-orange-400 flex items-center gap-1">
         <PiUserCircleFill />
         username
@@ -55,18 +54,18 @@ const Sidebar = () => {
       <div className="mt-10">
         <ul>
           {folders.map((folder) => (
-            <li key={folder.id} className="flex gap-2 items-center cursor-pointer p-0.5">
-              <Collapsible.Root>
-                <Collapsible.Trigger>
-                  <span className="flex items-center gap-2">
-                    <FaFolder /> {folder.name}
-                  </span>
+            <li key={folder.id}>
+              <Collapsible.Root defaultOpen>
+                <Collapsible.Trigger className="flex items-center gap-1 w-full justify-between py-0.5">
+                  <div className="flex items-center gap-1.5">
+                    <FaFolder className="text-sm" /> {folder.name}
+                  </div>
                 </Collapsible.Trigger>
                 <Collapsible.Content>
-                  <div className="ml-6 text-gray-300">
+                  <div className="ml-5 text-gray-300">
                     <ul className="text-sm">
                       {folder.user_rss_feeds.map((feed, index) => (
-                        <li key={index} className="hover:text-gray-400">
+                        <li key={index} className="text-gray-400 hover:text-gray-200 py-0.5">
                           <button onClick={() => getSite(feed.url)}>{feed.name}</button>
                         </li>
                       ))}
--- oberon-web/src/app/features/rss-feed/api/get-rss-feed.ts
+++ oberon-web/src/app/features/rss-feed/api/get-rss-feed.ts
@@ -4,22 +4,27 @@ import { RssFeed } from "@/app/features/rss-feed/types/types"
 
 const parseString = require("xml2js").parseString
 
-export async function getRssFeed(url: string) {
+type RssFeedResponse = {
+  rss: RssFeed
+}
+
+export async function getRssFeed(url: string): Promise<RssFeedResponse | null | undefined> {
   try {
     const response = await fetch(url)
-    if (!response) return
+    if (!response) return null
 
     const xmlString = await response.text()
     const parsedXML = await parseXMLResponse(xmlString)
+
     return parsedXML
   } catch (error) {
     console.log(error)
   }
 }
 
-function parseXMLResponse(xml: string): any {
+function parseXMLResponse(xml: string): Promise<RssFeedResponse | null> {
   return new Promise((resolve, reject) => {
-    parseString(xml, (error: Error, result: RssFeed) => {
+    parseString(xml, (error: Error, result: RssFeedResponse) => {
       if (error) {
         reject(error)
         return
