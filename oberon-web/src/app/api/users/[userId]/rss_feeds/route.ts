07500ecf1db2b0ca0c75a6aa64575ee0be5bd080
barbarabrito <barbaramabrito@gmail.com>
2024-05-16 23:23:58 -0300

    fix formatting

--- oberon-web/src/app/api/users/[userId]/rss_feeds/route.ts
+++ oberon-web/src/app/api/users/[userId]/rss_feeds/route.ts
@@ -1,11 +1,12 @@
 import supabase from "@//lib/supabase/supabase-client"
-import { PostgrestError, PostgrestResponse } from "@supabase/supabase-js"
+import { PostgrestResponse } from "@supabase/supabase-js"
 import { NextRequest, NextResponse } from "next/server"
 
 export async function POST(req: NextRequest, { params }: { params: { userId: string } }) {
-  if (req.method !== "POST") return NextResponse.json({ message: "Method not allowed" }, { status: 405 });
+  if (req.method !== "POST")
+    return NextResponse.json({ message: "Method not allowed" }, { status: 405 })
 
-  const data = await req.json();
+  const data = await req.json()
 
   if (!data.name || !data.url || !params.userId || !data.folder_id) {
     return NextResponse.json(
@@ -14,8 +15,8 @@ export async function POST(req: NextRequest, { params }: { params: { userId: str
       },
       {
         status: 400,
-      }
-    );
+      },
+    )
   }
 
   try {
@@ -23,12 +24,12 @@ export async function POST(req: NextRequest, { params }: { params: { userId: str
       data: feed,
       error: feedError,
     }: PostgrestResponse<{
-      id: number;
-      name: string;
-      url: string;
-      image_url: string;
-      created_at: string;
-      updated_at: string;
+      id: number
+      name: string
+      url: string
+      image_url: string
+      created_at: string
+      updated_at: string
     }> = await supabase
       .from("rss_feeds")
       .insert([
@@ -38,10 +39,10 @@ export async function POST(req: NextRequest, { params }: { params: { userId: str
           image_url: data.image_url ?? null,
         },
       ])
-      .select();
+      .select()
 
     if (feedError) {
-      return NextResponse.json({ status: 500 });
+      return NextResponse.json({ status: 500 })
     }
 
     if (feed) {
@@ -55,7 +56,6 @@ export async function POST(req: NextRequest, { params }: { params: { userId: str
     }
 
     return NextResponse.json(feed, { status: 201 })
-
   } catch (error) {
     return NextResponse.json({ status: 500 })
   }
